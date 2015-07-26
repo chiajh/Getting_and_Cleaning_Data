40ba617aebe41f299da8035a4abc57fb2e4bd0b4
chiajh <chiajh@gmail.com>
2015-07-26 15:26:58 +0800

    commited

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,6 @@
 # The data Source for this project is from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 # 1. Merges the training and the test sets to create one data set.
-
 file1 <- read.table("./train/X_train.txt")
 file2 <- read.table("./test/X_test.txt")
 X <- rbind(file1, file2)
@@ -18,7 +17,6 @@ Y <- rbind(file1, file2)
 
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-
 features <- read.table("features.txt")
 
 # Get the position of features with -mean() and -std() partial words. Escape (\\) the bracket.
@@ -29,7 +27,6 @@ X <- X[, index_mean_std]
 
 
 # 3. Uses descriptive activity names to name the activities in the data set.
-
 activities <- read.table("activity_labels.txt")
 
 # Change activity name to the lower case
@@ -39,26 +36,33 @@ names(Y) <- "activity"
 
 
 # 4. Appropriately labels the data set with descriptive variable names. 
-
 # Rename the X column names.
 names(X) <- features[index_mean_std, 2]
 
 # Remove the open and close bracket
 names(X) <- gsub("\\(|\\)", "", names(X)) 
 
-# change the column names of X to lower case
+# Change the column names of X to lower case
 names(X) <- tolower(names(X))
 
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
+# Rename column name for S
 names(S) <- "subject"
+
+# Create final tidy data set
 tidyData <- cbind(S, Y, X)
+
+# Write the final tidy data set to file
 write.table(tidyData, "merged_tidy_data.txt")
 
 library(data.table)
 dataDT <- data.table(tidyData)
-# mean function will be applied to sub-data.table, called '.SD' 
+
+# mean function applies to sub-data.table, called '.SD' 
 calculatedData<- dataDT[, lapply(.SD, mean), by=c("subject", "activity")]  
-write.table(calculatedData, "data_set_with_the_average.txt")
+
+# Write the calculated mean data set to file without row name
+write.table(calculatedData, "data_set_with_the_average.txt", row.name=FALSE)
+
 
